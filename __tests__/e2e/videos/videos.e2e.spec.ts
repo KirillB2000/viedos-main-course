19ee8d3caa22f01cc6d55a6a8078021bbefdd685
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-06-26 02:07:11 +0300

    'add-jest-config-and-fix-tsconfig-and-add-first-test'

--- __tests__/e2e/videos/videos.e2e.spec.ts
+++ __tests__/e2e/videos/videos.e2e.spec.ts
@@ -1,3 +1,10 @@
 import request from 'supertest';
 import { app } from "../../../src/index";
 
+describe("GET /", () => {
+  it("should return 'Hello world!'", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("Hello world!");
+  });
+});
