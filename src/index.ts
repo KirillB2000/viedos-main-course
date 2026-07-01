325e38a67603a452a89c57eea4575ab44158df18
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-07-01 03:08:58 +0300

    fix: vercel configuration 5

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import express from "express";
 import { setApp } from "./setup-app";
 
 // Создание приложения
-
 export const app = express();
 export default setApp(app);
 
 // Порт приложения
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000; // PORT?
-
-// Запуск приложения: выполняем ТОЛЬКО локально
-
-if (process.env.NODE_ENV !== 'production') {
+// Запуск приложения: Строгая проверка на окружение Vercel
+// Vercel ВСЕГДА автоматически внедряет переменную process.env.VERCEL
+if (!process.env.VERCEL) {
   app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
   });
